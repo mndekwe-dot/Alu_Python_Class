c9b34a76b4e8f3c7d5713b5fb5f50cecd7b79575
mndekwe-dot <m.ndekwe@alustudent.com>
2026-06-09 10:41:22 +0200

    Refactor Flask application structure and update requirements

--- Flask_Simple/main.py
+++ Flask_Simple/main.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
-app= Flask(__name__)
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello , Dieu Merci</h1>"
 
 @app.route("/hello")
 def hello_world():
     return "<h1>Hello , Dieu Merci</h1>"
-    
+
+if __name__ == "__main__":
+    app.run()
